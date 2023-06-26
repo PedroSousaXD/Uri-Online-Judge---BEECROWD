c823d1cd3dd0817bc826475e2667a665e9541883
Pedro Sousa <pedrocopom22@gmail.com>
2023-06-25 22:12:25 -0300

    Accepted: Diferença  #34312478

---
+++ Iniciante/diferenca.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int a, b, c, d, dif;
+  scanf("%d", &a);
+  scanf("%d", &b);
+  scanf("%d", &c);
+  scanf("%d", &d);
+  printf("DIFERENCA = %d\n", dif = ((a * b) - (c * d)));
+
+  return 0;
+}
