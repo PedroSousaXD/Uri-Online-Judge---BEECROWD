b0e820b4838c9988237a5d4f69900464ec856488
Pedro Sousa <pedrocopom22@gmail.com>
2023-06-25 21:57:10 -0300

    Accepted: Média 2 #34312103

---
+++ Iniciante/media2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double a, b, c, media;
+  scanf(" %lf", &a);
+  scanf(" %lf", &b);
+  scanf(" %lf", &c);
+  printf("MEDIA = %.1f\n", media =(( 2 * a ) + ( 3 * b )+( 5 * c ))/ 10);
+  return 0;
+}
