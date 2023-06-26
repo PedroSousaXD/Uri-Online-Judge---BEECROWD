e87efb090b265aede885c622c55ded76474687d0
Pedro Sousa <pedrocopom22@gmail.com>
2023-06-25 22:41:05 -0300

    Accepted: Salário #34313209

---
+++ Iniciante/salario.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+  
+  int a;
+  double b, c, x;
+  scanf("%d", &a);
+  scanf("%lf", &b);
+  scanf("%lf", &c);
+  printf("NUMBER = %d\nSALARY = U$ %.2f\n", a, x = b * c);
+
+  return 0;
+}
